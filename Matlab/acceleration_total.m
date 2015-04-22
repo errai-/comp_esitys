35e143700f43af20a518703ed25c37eb5fe613b0
taavita1 <aapo.taavitsainen@aalto.fi>
2015-04-22 19:08:28 +0300

    Lisätty paine kiihtyvyyden lausekkeeseen ja korjattu typo korottamalla tiheys toiseen potenssiin

--- Matlab/acceleration_total.m
+++ Matlab/acceleration_total.m
@@ -11,6 +11,7 @@ neighbors = cell(particle_count,1);
 
 densities = zeros(particle_count,1);
 accelerations = zeros(size(velocities));
+pressures = zeros(particle_count,1);
 
 %% Naive neighbor listing, takes into account overlap
 % TODO: intelligent neighbor listing
@@ -31,6 +32,16 @@ for i = 1:particle_count
 end
 display(densities);
 
+%% Calculate pressure
+% using Eq.3.38 and 3.91 from the arxiv article, for gamma the value was
+% taken from the book (usually used in HE simulations)
+gamma = 3;
+kappa = 1; % this entropic function is constant for isentropic flow (assumed)
+for i = 1:particle_count
+    internal_energy = kappa/(gamma - 1)*densities(i)^(gamma - 1);
+    pressures(i) = (gamma - 1)*densities(i)*internal_energy;
+end
+
 %% Calculate accelerations:
 % Loop over particles
 for i=1:particle_count
@@ -39,7 +50,7 @@ for i=1:particle_count
         if (j == i), continue; end
         r = calc_distance_2D(locations(j,:),locations(i,:));
         direction = (-locations(j,:)+locations(i,:))/r;
-        tmpAccel = m*direction*(1/densities(i)+1/densities(j))*...
+        tmpAccel = m*direction*(pressures(i)/densities(i)^2+pressures(j)/densities(j)^2)*...
             cubic_spline_kernel_gradient(r,h);
         accelerations(i,:) = accelerations(i,:) - tmpAccel;
         accelerations(j,:) = accelerations(j,:) + tmpAccel;
