3e663f26548a0bc9a6642ed8a39fab2636e5556a
Hannu Siikonen <hannu.siikonen@aalto.fi>
2015-04-22 18:36:18 +0300

    Jokseenkin järkevä perustavanlaatuinen simulaatiokeissi toimii nyt. Aiemmin puhutusta poiketen päädyin yhdistelemään aika monia osia yhteen funktioon, kun koodi tuntui silppuuntuvan turhan paljon.

--- Matlab/acceleration_total.m
+++ Matlab/acceleration_total.m
@@ -1,12 +1,49 @@
-function [ accelerations ] = acceleration_total( locations,velocities,h )
+function [ accelerations ] = acceleration_total( locations,velocities,h)
+% i) Finds a reasonable "important neighbors" listing
+% ii) Calculates densities
+% iii) Calculates finally accelerations, dumps all other temporary data
 
-accelerations = zeros(size(velocities));
+%% Initialization
+m = 1;
 particle_count = size(velocities,1);
 
-densities = density_total(locations,h);
+neighbors = cell(particle_count,1);
+
+densities = zeros(particle_count,1);
+accelerations = zeros(size(velocities));
+
+%% Naive neighbor listing, takes into account overlap
+% TODO: intelligent neighbor listing
 
 for i=1:particle_count
-    accelerations(i,:) = acceleration( locations, velocities, densities, h );
+    neighbors{i} = i:particle_count;
+end
+
+%% Calculate densities
+
+for i = 1:particle_count
+    for j=neighbors{i},
+        r = calc_distance_2D(locations(j,:),locations(i,:));
+        tmpDens = m*cubic_spline_kernel(r,h);
+        densities(i) = densities(i) + tmpDens;
+        densities(j) = densities(j) + tmpDens;
+    end
+end
+display(densities);
+
+%% Calculate accelerations:
+% Loop over particles
+for i=1:particle_count
+    % Loop over neighbors not yet handled
+    for j=neighbors{i},
+        if (j == i), continue; end
+        r = calc_distance_2D(locations(j,:),locations(i,:));
+        direction = (-locations(j,:)+locations(i,:))/r;
+        tmpAccel = m*direction*(1/densities(i)+1/densities(j))*...
+            cubic_spline_kernel_gradient(r,h);
+        accelerations(i,:) = accelerations(i,:) - tmpAccel;
+        accelerations(j,:) = accelerations(j,:) + tmpAccel;
+    end
 end
 
 end
--- Matlab/testcase.m
+++ Matlab/testcase.m
@@ -8,13 +8,13 @@ positions = [0,0;0,1;0,0.5;-0.5,0];
 velocities = [0,0;1,0;0,0;0,0];
 
 iter = 0;
-printLen = round(0.1/tStep);
+printLen = round(0.01/tStep);
 
 for tCurr = 0:tStep:tInterval
     % Loop logistics
     if ( mod(iter,printLen) == 0 )
         display(tCurr);
-        plot(positions,'*');
+        plot(positions(:,1),positions(:,2),'*');
         pause;
     end
     iter = iter+1;
