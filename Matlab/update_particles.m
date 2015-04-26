d44a15db9d9ad9c5f215b21a2256d3a5e06247ac
taavita1 <aapo.taavitsainen@aalto.fi>
2015-04-26 03:19:16 +0300

    modified for the variable time step

--- Matlab/update_particles.m
+++ Matlab/update_particles.m
@@ -1,14 +1,14 @@
-function [ locations, velocities, hVals ] = update_particles( locations, velocities, hVals, tStep,m,kappa,gamma,hConst )
+function [ locations, velocities, hVals, tStep] = update_particles( locations, velocities, hVals, tStep,m,kappa,gamma,hConst )
 
 % Calculate accelerations on the current step
-[accelerations,temp_hVals] = acceleration_total(locations, ...
-    velocities,hVals,m,kappa,gamma,tStep,hConst);
+[accelerations,temp_hVals,tStep] = acceleration_total(locations, ...
+    velocities,hVals,m,kappa,gamma,hConst);
 
 % Half timestep values
 temp_velocities = velocities+0.5*tStep*accelerations;
 temp_locations = locations+0.5*tStep*velocities;
 [accelerations,hVals] = acceleration_total(temp_locations, ...
-    temp_velocities,temp_hVals,m,kappa,gamma,tStep,hConst);
+    temp_velocities,temp_hVals,m,kappa,gamma,hConst);
 %display(hVals);
 % Final step values
 temp_velocities = velocities;
